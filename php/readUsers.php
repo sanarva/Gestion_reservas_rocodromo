8b9e2cfaf0c4cd60934592a538b8165e1d4a3d8b
Sandra Arcas <plosky21@hotmail.com>
2021-03-21 13:38:10 +0100

    Se añade toda la lógica en el listado de usuarios

---
+++ php/readUsers.php
@@ -0,0 +1,67 @@
+<?php  
+
+$path = "../views/userMenu.php";
+
+// Incluímos la conexión a la base de datos que está en el fichero database.php
+require "database.php";
+
+if (isset ($_POST["userNameFilter"])) {
+    $userNameFilter    = $_POST["userNameFilter"] . "%";
+    $userNameFilterGet = $_POST["userNameFilter"];
+} else {
+    $userNameFilter = "%";
+    $userNameFilterGet = "";
+}
+if (isset ($_POST["cardNumberFilter"])) {
+    $cardNumberFilter    = $_POST["cardNumberFilter"] . "%";
+    $cardNumberFilterGet = $_POST["cardNumberFilter"];
+} else {
+    $cardNumberFilter    = "%";
+    $cardNumberFilterGet = "";
+}
+
+if (isset ($_POST["checkAllUsersFilter"])) {
+    $checkAllUsersFilter = "%";
+    $checkAllUsersFilterGet = $_POST["checkAllUsersFilter"];
+} else {
+    $checkAllUsersFilter = "A%";
+    $checkAllUsersFilterGet = "";
+}
+
+
+//*********************************************************************************************//
+// Esta parte se encarga de cargar en la lista de usuarios, los usuarios de la BBDD            //
+//*********************************************************************************************//
+try {
+    $sql = "SELECT id_user, user_type, card_number, user_status, user_name, user_email 
+              FROM users
+             WHERE user_status LIKE :userstatus
+               AND user_name   LIKE :username
+               AND card_number LIKE :cardnumber
+          ORDER BY user_name";
+    $query = $conn->prepare($sql);
+    $query->bindParam(":userstatus",$checkAllUsersFilter);
+    $query->bindParam(":username",$userNameFilter);
+    $query->bindParam(":cardnumber",$cardNumberFilter);
+    $query->execute();
+    $users = $query->fetchAll(PDO::FETCH_OBJ);
+    //Si no existen usuarios, mostramos un aviso
+    if ($users == [] ){
+        $_SESSION['successFlag'] = "W";
+        $_SESSION['message'] = "No se han encontrado ningún usuario.";
+    }
+} catch(PDOException $e){
+    $_SESSION['successFlag'] = "C";
+    $queryError = $e->getMessage();  
+    $_SESSION['message'] = "Se ha detectado un problema a la hora de mostrar los usuarios. </br> Descripción del error: " . $queryError ; 
+} finally { 
+    //Limpiamos la memoria 
+    $conn = null;
+    if (isset ($_POST["userNameFilter"]) || isset ($_POST["cardNumberFilter"]) || isset ($_POST["checkAllUsersFilter"])) {
+        $_SESSION["searchUsers"] = $users;
+        header("Location: ../views/usersList.php?userName=$userNameFilterGet&cardNumber=$cardNumberFilterGet&checkAllUsersFilterGet=$checkAllUsersFilterGet");
+    }
+}
+
+?>
+
--- views/usersList.php
+++ views/usersList.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
-    <title>Listado usuarios</title>
+    <title>Usuarios</title>
 
     <!-- Estilos para la aplicación -->
     <!-- Estilos de Bootstrap 4-->
@@ -18,69 +18,117 @@
 </head>
 
 <body>
-  
+  <?php
+    $searchUserName         =  $_GET["userName"];
+    $searchCardNumber       =  $_GET["cardNumber"];
+    $checkAllUsersFilterGet =  $_GET["checkAllUsersFilterGet"];
+  ?>
+  <header>
+    <?php include("../php/header.php");?>
+  <header>
+
+  <h2>GESTIÓN DE USUARIOS</h2>
+
   <div class="container">
-      <p class= "closeSession"><i class="fas fa-sign-out-alt fa-lg"></i> Cerrar sesión</p> 
-      <h1>LISTADO DE USUARIOS</h1>
+    <!-- Aquí se muestran los campos para poder filtrar en la lista -->
+    <p class="border-bottom">Filtros:</p>
+    <form method="post" action="../php/readUsers.php" autocomplete="off" id="filterUsersForm" name="filterUsersForm">     
+      <div class="filterLayoutItems">
+        <label for="userNameFilter" class="col-form-label d-block"><i class="far fa-user"></i> Nombre usuario:</label>
+        <input type="text" name="userNameFilter" id="userNameFilter" value = <?php echo $searchUserName?>>
+      </div>
       
-      <p class="border-bottom">Filtros:</p>
-
-      <form action="" >
-                
-        <div class="filterLayoutItems">
-          <label for="userNameFilter" class="col-form-label d-block"><i class="far fa-user"></i> Nombre usuario:</label>
-          <input type="text" name="userNameFilter" id="userNameFilter">
-        </div>
-        
-        <div class="filterLayoutItems">
-          <label for="cardNumberFilter" class="col-form-label d-block"><i class="far fa-address-card"></i> Nº tarjeta:</label>
-          <input type="text" name="cardNumberFilter" id="cardNumberFilter">
-        </div>
-       
-        <div class="filterLayoutItems">
-          <input type="checkbox" name="checkAllUsers" id="checkAllUsers">
-          <label for="checkAllUsers" class="col-form-label">Mostrar usuarios inactivos</label>
-        </div>
-
-        <div class="row mt-2">
-          <div class="col-12">
-            <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Buscar</button>
-          </div>
-        </div>
-      </form>
-      
-      <table class="table  mt-5">
-        <thead>
-            <tr>
-              <th>NOMBRE USUARIO</th>
-              <th class="text-center">TIPO</th>
-              <th class="text-center">Nº TARJETA</th>
-              <th>EMAIL</th>
-              <th class="text-center">ESTADO</th>
-              <th class="text-center">OPCIONES</th>
-            </tr>
-        </thead>
-        <tbody>
-          <tr> <!--Por cada reserva que exista en la base de datos, tendrá que aparecer un registro-->
-            <td id="userNameUL"  name="userNameUL">Sandra Arcas Valero</td>
-            <td id="userTypeUL"  name="userTypeUL" class="text-center">Admin</td>
-            <td class="text-center" id="cardNumberUL"  name="cardNumberUL">0108</td>
-            <td id="emailUL"  name="emailUL">plosky21@hotmail.com</td>
-            <td id="userStatusUL"  name="userStatusUL" class="text-center"><i class="fas fa-check fa-lg text-success"></i></td>
-            <td class="d-flex justify-content-center">
-              <i class="fas fa-pen-square fa-lg text-info cursorHand mr-4"></i>
-              <i class="fas fa-times-circle fa-lg text-danger cursorHand"></i>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      
-      <div class="row">
+      <div class="filterLayoutItems">
+        <label for="cardNumberFilter" class="col-form-label d-block"><i class="far fa-address-card"></i> Nº tarjeta:</label>
+        <input type="text" name="cardNumberFilter" id="cardNumberFilter" value = <?php echo $searchCardNumber?>>
+      </div>
+     
+      <div class="filterLayoutItems">
+        <input type="checkbox" name="checkAllUsersFilter" id="checkAllUsersFilter"  <?php if ($checkAllUsersFilterGet == "on") {?> checked <?php } ?> >
+        <label for="checkAllUsersFilter" class="col-form-label">Mostrar usuarios inactivos</label>
+      </div>
+      <div class="row mt-2">
         <div class="col-12">
-          <a class="btn btn-primary" href="user.php">Crear usuario</a>
+          <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Buscar</button>
         </div>
       </div>
+    </form>
+    
+    <!-- Aquí se muestra la tabla donde ser verá la lista de usuarios -->
+    <table class="table  mt-5">
+      <thead>
+          <tr>
+            <th>NOMBRE USUARIO</th>
+            <th class="text-center">TIPO</th>
+            <th class="text-center">Nº TARJETA</th>
+            <th>EMAIL</th>
+            <th class="text-center">ESTADO</th>
+            <th class="text-center">OPCIONES</th>
+          </tr>
+      </thead>
+      <tbody>
+      <!--Llamamos a leer usuarios para cargar la lista de usuarios -->
+      <?php 
+        if ((!isset($_SESSION["searchUsers"]))){
+          include("../php/readUsers.php");
+        } else {
+          $users = $_SESSION["searchUsers"]; 
+        }
+      ?>
+        <!--Por cada reserva que exista en la base de datos, tendrá que aparecer un registro-->
+        <?php if(isset($users)){ foreach($users as $user):?>
+        <tr> 
+          <td id="userNameUL"  name="userNameUL"><?php echo $user->user_name?></td>
+          <td id="userTypeUL"  name="userTypeUL" class="text-center">
+          <?php if ($user->user_type == "A") { ?>
+            <i class="fas fa-user-cog" title="Admin"></i> 
+              <?php } else if ($user->user_type == "G"){ ?>
+                <i class="far fa-user text-secondary" title="Genérico"></i> 
+              <?php } ?>
+          </td>
+          <td class="text-center" id="cardNumberUL"  name="cardNumberUL"><?php echo $user->card_number?></td>
+          <td id="emailUL"  name="emailUL"><?php echo $user->user_email?></td>
+          <td id="userStatusUL"  name="userStatusUL" class="text-center">
+          <?php if ($user->user_status == "A") { ?>
+                <i class="fas fa-check text-success" title="Activo"></i> 
+              <?php } else if ($user->user_status == "I"){ ?>
+                <i class="fas fa-times text-danger" title="Inactivo"></i> 
+              <?php } ?>
+          </td>
+          <!--Botones Actualizar y Eliminar -->
+          <td class="d-flex justify-content-center">
+            <a href="user.php?Id=<?php echo $user->id_user?>&userName=<?php echo $user->user_name?>&userType=<?php echo $user->user_type?>&card_number=<?php echo $user->card_number?>&userEmail=<?php echo $user->user_email?>&userStatus=<?php echo $user->user_status?>">
+              <i title="Modificar" class="far fa-edit fa-lg cursorHand text-primary mr-4"></i>
+            </a>              
+            <a href="../php/deleteUser.php?Id=<?php echo $user->id_user?>&userName=<?php echo $user->user_name?>">
+              <i title="Eliminar" class="far fa-trash-alt fa-lg text-danger "></i>
+            </a> 
+          </td>
+        </tr>
+      <?php endforeach; ?> <?php }?>
+      </tbody>
+    </table>
+    
+    <div class="row">
+      <div class="col-12">
+        <a class="btn btn-primary" href="user.php">Crear usuario</a>
+      </div>
+    </div>
   </div>
+
+  <?php   
+    if ( isset($_SESSION['confirmation'])) {
+      include "../php/confirmation.php";
+    }
+
+    if (isset($_SESSION['successFlag'])) { 
+      include "../php/message.php";
+    }
+
+    if (isset($_SESSION["searchUsers"])){
+      unset ($_SESSION["searchUsers"]);
+    }
+  ?>
 
   <!-- Scripts para Bootstrap 4-->
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
