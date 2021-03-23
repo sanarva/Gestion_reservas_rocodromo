ca8e2256c0fac2d1f9d85d777aa021d82c76318b
Sandra Arcas <plosky21@hotmail.com>
2021-03-23 13:12:53 +0100

    Creamos un nuevo componente que será llamado cuando se listen las reservas y que lo que hará será desactivar las reservas cuando haya finalizado el día

---
+++ php/inactivateReservations.php
@@ -0,0 +1,20 @@
+<?php
+
+//Recuperamos la fecha actual
+$currentDate = date("Y-m-d");
+
+// Incluímos la conexión a la base de datos que está en el fichero database.php
+require "database.php";
+
+//Inactivamos todas las reservas que sean de días anteriores al actual
+$sql  =  "UPDATE reservations
+             SET reservation_status = 'I'  
+               , user_modification = '999999'
+           WHERE reservation_status = 'A'
+             AND reservation_date   < :currentdate";
+$query = $conn->prepare($sql);
+$query->bindParam(":currentdate", $currentDate);
+$query->execute();
+
+
+?>
